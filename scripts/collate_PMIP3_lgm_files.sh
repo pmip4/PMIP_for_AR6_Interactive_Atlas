c0d7f9377ee137a0afa7e2452d278c27d51d3828
Chris Brierley <c.brierley@ucl.ac.uk>
2021-02-04 18:23:58 +0000

    Collecting required metadata is proving rather awkward. Shifting to NCL..

--- scripts/collate_PMIP3_lgm_files.sh
+++ scripts/collate_PMIP3_lgm_files.sh
@@ -3,12 +3,34 @@
 DATA_DIR=/data/CMIP/curated_ESGF_replica
 
 PMIP3_MODELS="CCSM4 CNRM-CM5 COSMOS-ASO FGOALS-g2 GISS-E2-R IPSL-CM5A-LR MIROC-ESM MPI-ESM-P MRI-CGCM3"
+expt_names="lgm-cal-adj midHolocene-cal-adj"
 
+monClim="monClim_"
+
+tracking_ids=()
 #Pseudocode repeat for all desired variables. Making sure to select cal-adj or not appropriately
-for mod_i in $PMIP3_MODELS
+for expt_i in $expt_names
 do
-  ncclimo line
-  ncremap line
+  for var_i in "tas_"
+  do 
+    for mod_i in $PMIP3_MODELS
+    do
+      in_fils=`ls $DATA_DIR/$mod_i/lgm-cal-adj/$var_i*nc`
+      echo $in_fils
+      fil_str=${fil##*/}
+      fil_str_no_nc=${fil##*/}
+      for mm in {1..12}; do
+        ncra -O -F -d time,${mm},,12 $in_fils ../netcdfs/clm${mm}.nc
+      done
+      ncrcat -O ../netcdfs/clm*.nc ../netcdfs/$var_i$monClim$mod_i'_'$expt_i.nc
+      rm ../netcdfs/clm*.nc ../netcdfs/*.tmp
+      modellist+=($mod_i)
+      id=`ncdump -h ../netcdfs/$var_i$monClim$mod_i'_'$expt_i.nc | grep tracking`
+      tracking_ids+=(${id:18:36})      
+    done
+    echo $modellist
+    echo $tracking_ids
+  done
 done
-ncecat line
-ncks line to add model name as dimension
+#ncecat line
+#ncks line to add model name as dimension
